4aec60c78f5d0efa91bf02612b071aaeac231672
Matt Hough <matt@hough.tv>
2017-10-14 21:50:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,11 +7,16 @@ require_relative './course.rb'
 class Scraper
 
 def get_page
-  Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")).css(".post").each do |post|
+  Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 end
 
 def get_courses
+  get_page.css(".post").each do |post|
+  course = Course.new
+  course.title = post.css("h2").text
+  course.schedule = post.css(".date").text
+  course.description = post.css("p").text
 end
 
 def make_courses
