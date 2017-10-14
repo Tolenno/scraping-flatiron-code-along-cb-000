150c64b3316aa04f2f706744496d223ae664f06a
Matt Hough <matt@hough.tv>
2017-10-14 21:46:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,6 +10,8 @@ def get_page
   Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")).css(".post").each do |post|
     course = Course.new
     course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
 end
 
 def get_courses
