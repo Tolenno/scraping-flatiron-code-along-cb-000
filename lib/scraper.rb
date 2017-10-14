3c66352f9e70b44ac389722eb8a440c7cc904a03
Matt Hough <matt@hough.tv>
2017-10-14 21:23:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 class Scraper
 
 def get_page(url)
-  open("http://learn-co-curriculum.github.io/site-for-scraping/courses").css
+  Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")).css("")
 end
 
     def print_courses
